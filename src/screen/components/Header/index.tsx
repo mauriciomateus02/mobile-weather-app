e47ee2bcad3d1eab658ea4b0861d08d1e36c861f
mauriciomateus02 <mauriciomateus02@gmail.com>
2023-05-03 23:25:39 -0300

    adicinado comentarios superficiais para informar oq cada componente faz

--- src/screen/components/Header/index.tsx
+++ src/screen/components/Header/index.tsx
@@ -9,18 +9,21 @@ import Select from '../../../assets/Vector.svg';
 type HeaderProps = {
     text?: string;
 }
-
+// esse componente é a exibição do local da verificação do clima.
 const HeaderWeather = (props: HeaderProps) => {
     return (
         <Container style={{ width: '100%' }}>
-            <View style={styled.viewHeader}>
-                <View style={styled.viewMap}>
-                    <Map height={40} width={30} style={styled.conponent} />
-                    <Text style={styled.text}>{props.text}</Text>
-                    <Select width={15} height={15} style={styled.conponent} />
+                <View style={styled.viewHeader}>
+                    <View style={styled.viewMap}>
+                        {/*imagem de map*/}
+                        <Map height={40} width={30} style={styled.conponent} />
+                        {/*nome da cidade*/}
+                        <Text style={styled.text}>{props.text}</Text>
+                        {/*nome da botão de notificação*/}
+                        <Select width={15} height={15} style={styled.conponent} />
+                    </View>
+                    <Notification width={40} height={30} />
                 </View>
-                <Notification width={40} height={30} />
-            </View>
         </Container>
     )
 }
